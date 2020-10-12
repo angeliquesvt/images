ef4c5505664835cd5008a0ee052d34be06f8c993
user <user@localhost.localdomain>
2020-10-12 09:36:59 +0200

    tp4

--- main.py
+++ main.py
@@ -3,32 +3,37 @@ from sklearn.cluster import KMeans
 import cv2
 import glob
 
+def vectorisation():
+    return
+
 def main():
-    #siftDescripteurs = np.array()
+    #siftDescriptors = np.array()
     files = ['llama', 'pizza', 'octopus', 'lotus']
     N = 100
     first = True
+    kmeans = KMeans(n_clusters=N, random_state=0)
 
     for fn in files:
         for fileName in glob.glob('images/'+fn+'/train/*.jpg'):  #mettre train à la place de test
-            
-
             image = cv2.imread(fileName)
             greyImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
             sift = cv2.SIFT_create()
-            keypoints, descripteurs = sift.detectAndCompute(greyImage,None)
+            keypoints, descriptors = sift.detectAndCompute(greyImage,None)
             if first:
                 first = False
-                siftDescripteurs = np.array(descripteurs)
+                siftDescriptors = np.array(descriptors)
             else:
-                np.append(siftDescripteurs, np.array(descripteurs))
+                np.append(siftDescriptors, np.array(descriptors))
             #cv2.drawKeypoints(greyImage,keypoints,image)
             #cv2.imshow('sift_keypoints',image)
-    #arr d'ar, conv en np array en float
-    kmeans = KMeans(n_clusters=N, random_state=0).fit(siftDescripteurs)
+            #Z = np.random.rand(2000,2000)
+            #plt.imsave('center/%03i.png'%i,Z)
+
+    labels = kmeans.fit_predict(siftDescriptors)
     centers = kmeans.cluster_centers_
     np.savetxt('center.txt', centers)
-    print(centers.var())
+    
+    print(labels)
     cv2.waitKey(0)
 
 if __name__ == "__main__":
