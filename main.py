ffde46e2eaa14fe4f037707e8e380ce9c584e7de
user <user@localhost.localdomain>
2020-10-13 01:25:28 +0200

    tp4

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import numpy as np
 from sklearn.cluster import KMeans
 import cv2
 import glob
+import os, shutil, glob, os.path
+import matplotlib.pyplot as plt
 
 def vectorisation():
     return
@@ -11,7 +13,6 @@ def main():
     files = ['llama', 'pizza', 'octopus', 'lotus']
     N = 100
     first = True
-    kmeans = KMeans(n_clusters=N, random_state=0)
 
     for fn in files:
         for fileName in glob.glob('images/'+fn+'/train/*.jpg'):  #mettre train à la place de test
@@ -29,11 +30,24 @@ def main():
             #Z = np.random.rand(2000,2000)
             #plt.imsave('center/%03i.png'%i,Z)
 
-    labels = kmeans.fit_predict(siftDescriptors)
+    kmeans = KMeans(n_clusters=N, random_state=0).fit(siftDescriptors)
     centers = kmeans.cluster_centers_
     np.savetxt('center.txt', centers)
-    
-    print(labels)
+
+    totalVariance = np.zeros((500,))
+    for K in range(1,100):
+        clust = KMeans(n_clusters=K,n_init=3,verbose=0)
+        Ckm=clust.fit_predict(siftDescriptors)
+        totalVariance[K] = clust.inertia_
+        #print(J[K])
+
+    plt.plot(np.arange(1,500),totalVariance[1:500], color = 'red', linestyle = 'dashed', linewidth = 2, markerfacecolor = 'blue', markersize = 5)
+    plt.show()
+    #pplt.title('Variance totale')
+    #for i, m in enumerate(kmeans.labels_):
+    #    print("    Copy: %s / %s" %(i, len(kmeans.labels_)), end="\r")
+    #    shutil.copy(filelist[i], targetdir + str(m) + "_" + str(i) + ".jpg")
+
     cv2.waitKey(0)
 
 if __name__ == "__main__":
